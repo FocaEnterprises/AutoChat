dace8f4d9c68e72b5a7658eca2d5854a1bf565ee
GiverPlay007 <play.giver.007@gmail.com>
2024-03-17 23:42:17 -0300

    Inicialize LabyMod addon project

---
+++ src/main/java/me/giverplay/autochat/AutoChat.java
@@ -0,0 +1,20 @@
+package me.giverplay.autochat;
+
+import java.util.List;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+public class AutoChat extends LabyModAddon {
+  @Override
+  public void onEnable() {
+  }
+
+  @Override
+  protected void fillSettings(List<SettingsElement> settings) {
+  }
+  
+  @Override
+  public void loadConfig() {
+  }
+}
